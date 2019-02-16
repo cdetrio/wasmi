a69858ceac3c388af3badc3171b6b5a71e357386
Travis CI User <travis@example.org>
2019-02-16 12:55:04 +0000

    Update documentation

---
+++ aliases.js
@@ -0,0 +1,10 @@
+var ALIASES = {};
+ALIASES['byteorder'] = {};
+ALIASES['instantiate'] = {};
+ALIASES['instantiate'] = {};
+ALIASES['instantiate'] = {};
+ALIASES['memory_units'] = {};
+ALIASES['parity_wasm'] = {};
+ALIASES['wasmi'] = {};
+ALIASES['wasmi'] = {};
+ALIASES['wasmi'] = {};
